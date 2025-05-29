87779193398eca5b044d38a061f9031d156a43bb
Maksim Tetslav <maks.tetslav@yandex.ru>
2025-05-29 18:47:00 +0300

    feat: добавил тест для createId

---
+++ src/utils/tests/createId.test.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect, vi } from 'vitest';
+import { createId } from '../createId';
+
+describe(createId, () => {
+    it('Возвращает текущее время в миллисекундах', () => {
+        const fakeNow = 999;
+        vi.useFakeTimers();
+        vi.setSystemTime(fakeNow);
+
+        const id = createId();
+
+        expect(id).toBe(fakeNow);
+
+        vi.useRealTimers();
+    });
+
+    it('Возвращает число', () => {
+        const id = createId();
+        expect(typeof id).toBe('number');
+    });
+});
