c8cde1f064f6f7647be75c9f152c3d18cdab3860
Maksim Tetslav <maks.tetslav@yandex.ru>
2025-05-29 19:33:17 +0300

    feat: добавил тесты для TextInput

---
+++ src/components/TextInput/TextInput.test.tsx
@@ -0,0 +1,62 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { TODO_INPUT_PLACEHOLDER } from '@Entities/todos';
+
+import TextInput from './';
+
+const setupTest = () => {
+    const user = userEvent.setup();
+    const fakeFn = vi.fn();
+    const { getByRole, getByLabelText } = render(<TextInput onAddTodo={fakeFn} />);
+
+    return {
+        getByRole,
+        getByLabelText,
+        fakeFn,
+        user
+    };
+};
+
+describe(TextInput, () => {
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
+
+    it('Рендерит инпут', () => {
+        const { getByLabelText } = setupTest();
+
+        expect(getByLabelText(TODO_INPUT_PLACEHOLDER)).toBeInTheDocument();
+    });
+
+    it('вводит текст и вызывает onAddTodo при нажатии Enter', async () => {
+        const { getByRole, fakeFn, user } = setupTest();
+
+        const inputText = 'Устроиться на работу в mindbox';
+
+        const input = getByRole('textbox') as HTMLInputElement;
+        await user.type(input, `${inputText}{enter}`);
+
+        expect(fakeFn).toHaveBeenCalledTimes(1);
+        expect(fakeFn).toHaveBeenCalledWith(inputText);
+        expect(input.value).toBe('');
+    });
+
+    it('не вызывает onAddTodo при нажатии Enter, если поле пустое', async () => {
+        const { getByRole, fakeFn, user } = setupTest();
+
+        const input = getByRole('textbox');
+        await user.type(input, '{enter}');
+
+        expect(fakeFn).not.toHaveBeenCalled();
+    });
+
+    it('не вызывает onAddTodo, если введена строка только из пробелов', async () => {
+        const { getByRole, fakeFn, user } = setupTest();
+
+        const input = getByRole('textbox');
+        await user.type(input, '     {enter}');
+
+        expect(fakeFn).not.toHaveBeenCalled();
+    });
+});
