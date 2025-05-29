33dc4ec4abfb8e3f6562f7e6ff7388448ab7803a
Maksim Tetslav <maks.tetslav@yandex.ru>
2025-05-29 18:47:10 +0300

    feat: добавил тест для Divider

---
+++ src/components/Divider/Divider.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import Divider from './';
+
+describe(Divider, () => {
+    it('Рендерит разделитель', () => {
+        const { getByRole } = render(<Divider />);
+
+        const divider = getByRole('separator');
+        expect(divider).toBeInTheDocument();
+    });
+});
