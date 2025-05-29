6aa4a6b842e1ebce67bcc81f922500013983f9f6
Maksim Tetslav <maks.tetslav@yandex.ru>
2025-05-29 21:06:30 +0300

    feat: изменил логику useFilterTodos

--- src/hooks/__tests__/useFilterTodos.test.tsx
+++ src/hooks/__tests__/useFilterTodos.test.tsx
@@ -2,43 +2,43 @@ import type { SyntheticEvent } from 'react';
 import { renderHook, act } from '@testing-library/react';
 
 import { TODO_TYPES, DEFAULT_TODO_TYPE } from '@entities/todos';
-import { fakeCompletedTodo, fakeIncompletedTodos, fakeTodoList } from '@utils/__mocks__/todo';
 
 import { useFilterTodos } from '../useFilterTodos';
 
 const setupTest = () => {
-    const { result } = renderHook(() => useFilterTodos({ todoList: fakeTodoList }));
+    const { result } = renderHook(() => useFilterTodos());
 
     return { result };
 };
 
 describe(useFilterTodos, () => {
-    it('Возвращает все todo', () => {
+    it('Возвращает значение по умолчанию', () => {
         const { result } = setupTest();
 
         expect(result.current.currentFilter).toBe(DEFAULT_TODO_TYPE);
-        expect(result.current.filteredTodos).toEqual(fakeTodoList);
     });
 
-    it('Возвращает только активные todo', () => {
+    it('Меняет фильтр при вызове handleFilterChange', () => {
         const { result } = setupTest();
 
+        const fakeEvent = {} as SyntheticEvent;
+
         act(() => {
-            result.current.handleFilterChange({} as SyntheticEvent, TODO_TYPES.ACTIVE);
+            result.current.handleFilterChange(fakeEvent, TODO_TYPES.ACTIVE);
         });
 
         expect(result.current.currentFilter).toBe(TODO_TYPES.ACTIVE);
-        expect(result.current.filteredTodos).toEqual(fakeIncompletedTodos);
-    });
-
-    it('Возвращает только завершённые todo', () => {
-        const { result } = setupTest();
 
         act(() => {
-            result.current.handleFilterChange({} as SyntheticEvent, TODO_TYPES.COMPLETED);
+            result.current.handleFilterChange(fakeEvent, TODO_TYPES.COMPLETED);
         });
 
         expect(result.current.currentFilter).toBe(TODO_TYPES.COMPLETED);
-        expect(result.current.filteredTodos).toEqual([fakeCompletedTodo]);
+
+        act(() => {
+            result.current.handleFilterChange(fakeEvent, TODO_TYPES.ALL);
+        });
+
+        expect(result.current.currentFilter).toBe(TODO_TYPES.ALL);
     });
 });
--- src/hooks/useFilterTodos.tsx
+++ src/hooks/useFilterTodos.tsx
@@ -1,46 +1,22 @@
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import type { SyntheticEvent } from 'react';
 
-import type { Todos, TodoType } from '@entities/todos';
-import { DEFAULT_TODO_TYPE, TODO_TYPES } from '@entities/todos';
-
-interface UseFilterTodosProps {
-    todoList: Todos;
-}
+import type { TodoType } from '@entities/todos';
+import { DEFAULT_TODO_TYPE } from '@entities/todos';
 
 interface UseFilterTodosResult {
     currentFilter: TodoType;
-    filteredTodos: Todos;
     handleFilterChange: (_: SyntheticEvent, value: TodoType) => void;
 }
 
-export const useFilterTodos = ({ todoList }: UseFilterTodosProps): UseFilterTodosResult => {
+export const useFilterTodos = (): UseFilterTodosResult => {
     const [currentFilter, setCurrentFilter] = useState<TodoType>(DEFAULT_TODO_TYPE);
-
-    const filteredTodos = useMemo(
-        () =>
-            todoList.filter((todo) => {
-                switch (currentFilter) {
-                    case TODO_TYPES.ACTIVE: {
-                        return !todo.completed;
-                    }
-                    case TODO_TYPES.COMPLETED: {
-                        return todo.completed;
-                    }
-                    default: {
-                        return true;
-                    }
-                }
-            }),
-        [todoList, currentFilter]
-    );
 
     const handleFilterChange = (_: SyntheticEvent, value: TodoType): void => {
         setCurrentFilter(value);
     };
 
     return {
-        filteredTodos,
         currentFilter,
         handleFilterChange
     };
