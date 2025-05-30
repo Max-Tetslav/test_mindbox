9b03002af494338450bd3b8a264a3f09479bb11a
Maksim Tetslav <maks.tetslav@yandex.ru>
2025-05-30 17:30:54 +0300

    feat: добавил import плагины для eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,8 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
     { ignores: ['dist', 'coverage'] },
@@ -18,12 +20,33 @@ export default tseslint.config(
         plugins: {
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
+            'unused-imports': eslintPluginUnusedImports,
+            import: eslintPluginImport,
             prettier: eslintPluginPrettier
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
-            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-            'prettier/prettier': 'error'
+            'react-refresh/only-export-components': ['error', { allowConstantExport: true }],
+            'prettier/prettier': 'error',
+            'unused-imports/no-unused-imports': 'error',
+            'unused-imports/no-unused-vars': [
+                'error',
+                {
+                    vars: 'all',
+                    varsIgnorePattern: '^_',
+                    args: 'after-used',
+                    argsIgnorePattern: '^_'
+                }
+            ],
+            'import/order': [
+                'error',
+                {
+                    groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+                    'newlines-between': 'always',
+                    alphabetize: { order: 'asc', caseInsensitive: false }
+                }
+            ],
+            'import/no-duplicates': 'error'
         },
         languageOptions: {
             parserOptions: {
