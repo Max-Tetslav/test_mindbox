d33d50a505100a79007f4afd79b57a6af7095180
Maksim Tetslav <maks.tetslav@yandex.ru>
2025-06-01 17:25:42 +0300

    feat: добавил ограничение по длинне строки

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,7 +1,8 @@
 import { TextField } from '@mui/material';
 import { memo, useCallback, useState, type ChangeEvent } from 'react';
 
-import { TODO_INPUT_PLACEHOLDER } from '@entities/todos';
+import { TODO_INPUT_ERROR_TEXT, TODO_INPUT_PLACEHOLDER } from '@entities/Todo/model/todos';
+import { useInputLengthLimit } from '@hooks/useLimitText';
 
 type TextInputProps = {
     onAddTodo: (todoText: string) => void;
@@ -10,10 +11,13 @@ type TextInputProps = {
 const TextInput = memo(({ onAddTodo }: TextInputProps) => {
     const [value, setValue] = useState<string>('');
 
+    const { isInputLimitReached } = useInputLengthLimit({ inputLength: value.length });
+
     const handleAddTodo = (e: React.KeyboardEvent<HTMLInputElement>) => {
         const noSpacesValue = value.trim();
+        const canAddNewTodo = e.key === 'Enter' && noSpacesValue && !isInputLimitReached;
 
-        if (e.key === 'Enter' && noSpacesValue) {
+        if (canAddNewTodo) {
             onAddTodo(noSpacesValue);
             setValue('');
         }
@@ -21,15 +25,17 @@ const TextInput = memo(({ onAddTodo }: TextInputProps) => {
 
     const handleChangeInput = useCallback((e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value), []);
 
+    const inputLabelText = isInputLimitReached ? TODO_INPUT_ERROR_TEXT : TODO_INPUT_PLACEHOLDER;
+
     return (
         <TextField
             variant="outlined"
-            label={TODO_INPUT_PLACEHOLDER}
+            label={inputLabelText}
             value={value}
             sx={{ mb: 2 }}
             onKeyDown={handleAddTodo}
             onChange={handleChangeInput}
-            fullWidth
+            error={isInputLimitReached}
         />
     );
 });
