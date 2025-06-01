79cf92ad10115ac0ae6ce45d2f5d1eea83de2533
Maksim Tetslav <maks.tetslav@yandex.ru>
2025-06-02 01:35:38 +0300

    feat: добавил тест ошибки в TextInput

--- src/components/TextInput/TextInput.test.tsx
+++ src/components/TextInput/TextInput.test.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { TODO_INPUT_PLACEHOLDER } from '@entities/Todo/model/todos';
+import { TODO_INPUT_ERROR_TEXT, TODO_INPUT_PLACEHOLDER, TODO_SYMBOLS_LIMIT } from '@entities/Todo/model/todos';
 import { fakeCompletedTodo } from '@shared/lib/__mocks__/todo';
 
-import TextInput from './TextInput';
+import { TextInput } from './TextInput';
 
 const setupTest = () => {
     const user = userEvent.setup();
@@ -57,5 +57,14 @@ describe(TextInput, () => {
         await user.type(input, '     {enter}');
 
         expect(fakeFn).not.toHaveBeenCalled();
+    });
+
+    it('Отображает ошибку при превышении лимита символов', async () => {
+        const { getByRole, getByLabelText, user } = setupTest();
+
+        const input = getByRole('textbox');
+
+        await user.type(input, '1'.repeat(TODO_SYMBOLS_LIMIT + 1));
+        expect(getByLabelText(TODO_INPUT_ERROR_TEXT)).toBeInTheDocument();
     });
 });
