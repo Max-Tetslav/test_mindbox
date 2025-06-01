f88aef94607e2862e7022f26cb611764185d66ef
Maksim Tetslav <maks.tetslav@yandex.ru>
2025-06-01 16:51:52 +0300

    feat: добавил todo entity

--- src/entities/Todo/model/todos.ts
+++ src/entities/Todo/model/todos.ts
@@ -17,3 +17,5 @@ export const DEFAULT_TODO_TYPE = TODO_TYPES.ALL;
 export type TodoType = (typeof TODO_TYPES)[keyof typeof TODO_TYPES];
 
 export const TODO_INPUT_PLACEHOLDER = 'What needs to be done?';
+export const TODO_INPUT_ERROR_TEXT = 'Maximum 40 symbols';
+export const TODO_SYMBOLS_LIMIT = 40;
